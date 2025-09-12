d143dd66292d67b634e22ca378ee51f84c979e02
WowNicky <nickykok.wow@gmail.com>
2025-09-12 19:15:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ WHITELIST_WS_NAME = "Whitelist"
 
 # Malaysia = UTC+8
 MYT = timezone(timedelta(hours=8))
-OPEN_AT = datetime(2025, 9, 12, 19, 10, 0, tzinfo=MYT)   # <<< your opening time
+OPEN_AT = datetime(2025, 9, 12, 19, 20, 0, tzinfo=MYT)   # <<< your opening time
 AUTO_REFRESH_MS_BEFORE = 1000  # 1s refresh before open (countdown)
 AUTO_REFRESH_MS_AFTER  = 2000  # 2s refresh after open (live seat updates)
 
@@ -548,3 +548,4 @@ if st.session_state.get("auth_ok", False):
             del st.session_state[k]
         # replaced experimental API with stable API
         st.rerun()
+
